b854f9237ac319718dff2159dffdbc602465fe91
tvdinh <tvdinh@tma.com.vn>
2017-09-09 15:03:46 +0700

    Add rest controller

--- RestService/src/main/java/ems/controller/GreetingController.java
+++ RestService/src/main/java/ems/controller/GreetingController.java
@@ -4,9 +4,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import ems.model.Greeting;
 
+@RestController
 public class GreetingController {
     private static final String template = "Hello, %s";
     private final AtomicInteger counter = new AtomicInteger();
