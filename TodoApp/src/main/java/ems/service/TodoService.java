18313ad8b193a91262f8641585508477ea177913
tvdinh <tvdinh@tma.com.vn>
2017-09-11 14:03:01 +0700

    Add Todo service class

---
+++ TodoApp/src/main/java/ems/service/TodoService.java
@@ -0,0 +1,37 @@
+package ems.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import ems.model.Todo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+    private static List<Todo> todos = new ArrayList<Todo>();
+    private static int todoCount = 3;
+
+    static {
+        todos.add( new Todo( 1, "tvdinh", "Learn Spring MVC", new Date(), false ) );
+        todos.add( new Todo( 2, "tvdinh", "Learn Strut", new Date(), false ) );
+        todos.add( new Todo( 3, "tvdinh", "Learn Hibernate", new Date(), false ) );
+    }
+
+    public List<Todo> getAll() {
+        return todos;
+    }
+
+    public List<Todo> getAll( String user ) {
+        List<Todo> result = new ArrayList<Todo>();
+        if ( user != null ) {
+            for ( Todo todo : todos ) {
+                if ( user.equals( todo.getUser() ) ) {
+                    result.add( todo );
+                }
+            }
+        }
+        return result;
+    }
+}
