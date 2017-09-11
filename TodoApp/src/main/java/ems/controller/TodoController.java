ed9496f858119887680fc45c3887f1c421a06ce3
tvdinh <tvdinh@tma.com.vn>
2017-09-11 14:07:59 +0700

    Add todo controller

---
+++ TodoApp/src/main/java/ems/controller/TodoController.java
@@ -0,0 +1,29 @@
+package ems.controller;
+
+import ems.service.TodoService;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TodoController {
+    private TodoService todoService;
+
+    @RequestMapping( value = "/list-todo", method = RequestMethod.GET )
+    public String showTodoList( ModelMap model ) {
+        model.addAttribute( "todos", todoService.getAll( getUsername() ) );
+        return "list-todo";
+    }
+
+    private String getUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication();
+        if ( principal instanceof UserDetails ) {
+            return ((UserDetails) principal).getUsername();
+        }
+        return principal.toString();
+    }
+}
